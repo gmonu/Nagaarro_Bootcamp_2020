a4b48cb9ac66bafa1afb1ce20984304fece454ac
= <=>
2020-02-14 01:11:50 +0530

    added new Program

---
+++ src/session_7/Magic_Pattern.java
@@ -0,0 +1,19 @@
+package session_7;
+
+import java.util.Scanner;
+
+public class Magic_Pattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		for(int i = 0; i < n; i++)
+		{
+			
+		}
+	}
+
+}
